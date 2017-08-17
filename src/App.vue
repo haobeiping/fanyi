452b79eb99c94597babbcc4a2efd83acd968923c
haobeiping <578230429@qq.com>
2017-08-17 15:22:14 +0800

    修饰

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
   <h1>在线翻译</h1>
-  <h5>简单 / 易用 /便捷</h5>
+  <h5 class="text-muted">简单 / 易用 /便捷</h5>
   <translateForm @formSubmit="translateText"></translateForm>
   <translateOutput v-text="translatedText"></translateOutput>
   </div>
--- src/components/TranslateForm.vue
+++ src/components/TranslateForm.vue
@@ -1,16 +1,18 @@
 <template>
-  <div id="translateForm">
-    <form @submit="formSubmit">
-      <input type="text" placeholder="输入需要翻译的内容" v-model="textToTranslate">
-      <select v-model="language">
-        <option value="en">English(英语)</option>
-        <option value="ru">Russian(俄语)</option>
-        <option value="ko">korean(韩语)</option>
-        <option value="de">German(德语)</option>
-        <option value="fr">French(法语)</option>
-      </select>
-      <input type="submit" value="翻译">
-    </form>
+  <div class="row" id="translateForm" >
+    <div class="col-md-6 col-md-offset-3">
+      <form id="tranForm" class="well form-inline" @submit="formSubmit">
+        <input class="form-control" type="text" placeholder="输入需要翻译的内容" v-model="textToTranslate">
+        <select class="form-control form-inline" v-model="language">
+          <option value="en">English(英语)</option>
+          <option value="ru">Russian(俄语)</option>
+          <option value="ko">korean(韩语)</option>
+          <option value="de">German(德语)</option>
+          <option value="fr">French(法语)</option>
+        </select>
+        <input class="btn btn-primary" type="submit" value="翻译">
+      </form>
+    </div>
   </div>
 </template>
 
@@ -35,5 +37,8 @@ export default {
 </script>
 
 <style>
-
+#transForm{
+  border:1px #ccc solid;
+  border:1px #ccc solid;
+}
 </style>
